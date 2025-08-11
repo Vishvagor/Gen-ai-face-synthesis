4f31642d4dffa67b0cd9cf8e5f4ec173ab9d1c14
Vishvagor <144877411+Vishvagor@users.noreply.github.com>
2025-08-11 12:23:34 -0700

    Delete src directory

--- src/inference.py
+++
@@ -1,8 +0,0 @@
-# Put your real diffusion/GAN sampler here.
-# Example contract:
-#   def run_inference(seed: int = 42):
-#       # load model/weights (cache globally for speed)
-#       # set seed
-#       # sample a 256x256 image
-#       # return a PIL.Image
-#       raise NotImplementedError("Implement your diffusion/GAN inference here.")
--- src/utils.py
+++
@@ -1,1 +0,0 @@
-# Put preprocessing / postprocessing helpers here.
